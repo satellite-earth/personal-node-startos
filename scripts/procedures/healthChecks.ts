4d079f2595dfda178b8f305f03f291bb4cbb9615
hzrd149 <github@hzrd149.com>
2024-08-15 11:50:03 -0500

    bump modules

--- scripts/procedures/healthChecks.ts
+++ scripts/procedures/healthChecks.ts
@@ -2,6 +2,6 @@ import { types as T, healthUtil } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
   async "web-ui"(effects, duration) {
-    return healthUtil.checkWebUrl("http://satellite-personal-node.embassy:80")(effects, duration).catch(healthUtil.catchError(effects))
+    return healthUtil.checkWebUrl("http://satellite-personal-node.embassy:80/health")(effects, duration).catch(healthUtil.catchError(effects))
   },
 };
