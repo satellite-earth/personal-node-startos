731cc01804e948fc5e5a5b6a71dac34230bdeab3
hzrd149 <github@hzrd149.com>
2024-09-09 15:49:19 -0500

    pass tor address to node

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,4 +2,6 @@
 
 cd /app/personal-node
 
+export TOR_ADDRESS=$(yq e '.tor-address' /root/start9/config.yaml)
+
 node .
--- scripts/procedures/getConfig.ts
+++ scripts/procedures/getConfig.ts
@@ -1,5 +1,15 @@
 // To utilize the default config system built, this file is required. It defines the *structure* of the configuration file. These structured options display as changeable UI elements within the "Config" section of the service details page in the StartOS UI.
 
-import { compat, types as T } from "../deps.ts";
+import { compat } from "../deps.ts";
 
-export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({});
+export const [getConfig, setConfigMatcher] = compat.getConfigAndMatcher({
+	'tor-address': {
+    name: "Tor Address",
+    description: "The Tor address of the node and relay interface",
+    type: "pointer",
+    subtype: "package",
+    "package-id": "satellite-personal-node",
+    target: "tor-address",
+    interface: "main",
+	}
+});
